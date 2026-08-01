a087ec83f1c281a1d728d99a3140efa4f59426b1
T0V6 <89317713+T0V6@users.noreply.github.com>
2026-08-01 00:15:01 -0300

    feat: add [C/CXX]NN definition macros for lang version

--- include/std_v.h
+++ include/std_v.h
@@ -15,48 +15,54 @@
     #define CXX_STD STR(_COMPILER_CXX_STD)
 
 
-    #define CXX11   201103L //0x (1x)
-    #define CXX14   201402L //1y
-    #define CXX17   201703L //1z
-    #define CXX20   202002L //2a
-    #define CXX23   202302L //2b
-    #define CXX26   202603L //2c
+    #define _STD_CXX11   201103L //0x (1x)
+    #define _STD_CXX14   201402L //1y
+    #define _STD_CXX17   201703L //1z
+    #define _STD_CXX20   202002L //2a
+    #define _STD_CXX23   202302L //2b
+    #define _STD_CXX26   202603L //2c
 
 
-    #if _COMPILER_CXX_STD < CXX11
+    #if _COMPILER_CXX_STD < _STD_CXX11
         #define CXX_STD_NAME    "preCXX11"
 
-    #elif _COMPILER_CXX_STD == CXX11
+    #elif _COMPILER_CXX_STD == _STD_CXX11
+        #define CXX11
         #define CXX_STD_NAME    "CXX11"
 
-    #elif _COMPILER_CXX_STD < CXX14
+    #elif _COMPILER_CXX_STD < _STD_CXX14
         #define CXX_STD_NAME    "preCXX14"
 
-    #elif _COMPILER_CXX_STD == CXX14
+    #elif _COMPILER_CXX_STD == _STD_CXX14
+        #define CXX14
         #define CXX_STD_NAME    "CXX14"
 
-    #elif _COMPILER_CXX_STD < CXX17
+    #elif _COMPILER_CXX_STD < _STD_CXX17
         #define CXX_STD_NAME    "preCXX17"
 
-    #elif _COMPILER_CXX_STD == CXX17
+    #elif _COMPILER_CXX_STD == _STD_CXX17
+        #define CXX17
         #define CXX_STD_NAME    "CXX17"
 
-    #elif _COMPILER_CXX_STD < CXX20
+    #elif _COMPILER_CXX_STD < _STD_CXX20
         #define CXX_STD_NAME    "preCXX20"
 
-    #elif _COMPILER_CXX_STD == CXX20
+    #elif _COMPILER_CXX_STD == _STD_CXX20
+        #define CXX20
         #define CXX_STD_NAME    "CXX20"
 
-    #elif _COMPILER_CXX_STD < CXX23
+    #elif _COMPILER_CXX_STD < _STD_CXX23
         #define CXX_STD_NAME    "preCXX23"
 
-    #elif _COMPILER_CXX_STD == CXX23
+    #elif _COMPILER_CXX_STD == _STD_CXX23
+        #define CXX23
         #define CXX_STD_NAME    "CXX23"
 
-    #elif _COMPILER_CXX_STD < CXX26
+    #elif _COMPILER_CXX_STD < _STD_CXX26
         #define CXX_STD_NAME    "preCXX26"
 
-    #elif _COMPILER_CXX_STD == CXX26
+    #elif _COMPILER_CXX_STD == _STD_CXX26
+        #define CXX26
         #define CXX_STD_NAME    "CXX26"
 
     #else
@@ -81,42 +87,47 @@
     #define C_STD STR(_COMPILER_C_STD)
 
 
-    #define C99     199901L // (0X)
-    #define C11     201112L // 1X
-    #define C17     201710L // 1X
-    #define C23     202311L // 2X
+    #define _STD_C99     199901L // (0X)
+    #define _STD_C11     201112L // 1X
+    #define _STD_C17     201710L // 1X
+    #define _STD_C23     202311L // 2X
     // todo! update with final value
-    #define C29     202911L // 2Y
+    #define _STD_C29     202911L // 2Y
 
 
-    #if _COMPILER_C_STD < C99
+    #if _COMPILER_C_STD < _STD_C99
         #define C_STD_NAME  "preC99"
 
-    #elif _COMPILER_C_STD == C99
+    #elif _COMPILER_C_STD == _STD_C99
+        #define C99
         #define C_STD_NAME  "C99"
 
-    #elif _COMPILER_C_STD < C11
+    #elif _COMPILER_C_STD < _STD_C11
         #define C_STD_NAME  "preC11"
 
-    #elif _COMPILER_C_STD == C11
+    #elif _COMPILER_C_STD == _STD_C11
+        #define C11
         #define C_STD_NAME  "C11"
 
-    #elif _COMPILER_C_STD < C17
+    #elif _COMPILER_C_STD < _STD_C17
         #define C_STD_NAME  "preC17"
 
-    #elif _COMPILER_C_STD == C17
+    #elif _COMPILER_C_STD == _STD_C17
+        #define C17
         #define C_STD_NAME  "C17"
 
-    #elif _COMPILER_C_STD < C23
+    #elif _COMPILER_C_STD < _STD_C23
         #define C_STD_NAME  "preC23"
 
-    #elif _COMPILER_C_STD == C23
+    #elif _COMPILER_C_STD == _STD_C23
+        #define C23
         #define C_STD_NAME  "C23"
 
-    #elif _COMPILER_C_STD < C29
+    #elif _COMPILER_C_STD < _STD_C29
         #define C_STD_NAME  "preC29"
 
-    #elif _COMPILER_C_STD == C29
+    #elif _COMPILER_C_STD == _STD_C29
+        #define C29
         #define C_STD_NAME  "C29"
 
     #else
