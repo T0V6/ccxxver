e7218e463a7ce21e5d2a7da379550bafedf05d3b
T0V6 <89317713+T0V6@users.noreply.github.com>
2026-07-31 23:45:41 -0300

    refac: make hash and branch strings

--- include/version.h
+++ include/version.h
@@ -3,10 +3,8 @@
 
 #define VERSION_COUNT   0
     #define VERSION_COUNT_STR   "0"
-#define VERSION_HASH    0
-    #define VERSION_HASH_STR    "0"
-#define VERSION_BRANCH  0
-    #define VERSION_BRANCH_STR  "0"
+#define VERSION_HASH    "0"
+#define VERSION_BRANCH  "0"
 #define VERSION_COUNT_OFFSET_LAST   0
     #define VERSION_COUNT_OFFSET_LAST_STR   "0"
 #define VERSION_TIMESTAMP   0
@@ -16,11 +14,11 @@
 #define VERSION_STR (\
     VERSION_COUNT_STR \
     "." \
-    VERSION_HASH_STR \
+    VERSION_HASH \
     "." \
     VERSION_TIMESTAMP_STR \
     "-" \
-    VERSION_BRANCH_STR \
+    VERSION_BRANCH \
     "." \
     VERSION_COUNT_OFFSET_LAST_STR \
 )
